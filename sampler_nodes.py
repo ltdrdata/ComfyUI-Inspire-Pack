9607c9578f70eef1821c80883f047183b6f14b08
Dr.Lt.Data <dr.lt.data@gmail.com>
2023-10-13 00:20:59 +0900

    fix: typo

--- sampler_nodes.py
+++ sampler_nodes.py
@@ -18,7 +18,7 @@ class KSampler_progress(a1111_compat.KSampler_inspire):
                      "denoise": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01}),
                      "noise_mode": (["GPU(=A1111)", "CPU"],),
                      "interval": ("INT", {"default": 1, "min": 1, "max": 10000}),
-                     "ommit_start_latent": ("BOOLEAN", {"default": True, "label_on": "True", "label_off": "False"}),
+                     "omit_start_latent": ("BOOLEAN", {"default": True, "label_on": "True", "label_off": "False"}),
                      }
                 }
 
@@ -27,12 +27,12 @@ class KSampler_progress(a1111_compat.KSampler_inspire):
     RETURN_TYPES = ("LATENT", "LATENT")
     RETURN_NAMES = ("latent", "progress_latent")
 
-    def sample(self, model, seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise, noise_mode, interval, ommit_start_latent):
+    def sample(self, model, seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise, noise_mode, interval, omit_start_latent):
         adv_steps = int(steps / denoise)
 
         sampler = a1111_compat.KSamplerAdvanced_inspire()
 
-        if ommit_start_latent:
+        if omit_start_latent:
             result = []
         else:
             result = [latent_image['samples']]
@@ -72,7 +72,7 @@ class KSamplerAdvanced_progress(a1111_compat.KSamplerAdvanced_inspire):
                      "noise_mode": (["GPU(=A1111)", "CPU"],),
                      "return_with_leftover_noise": ("BOOLEAN", {"default": False, "label_on": "enable", "label_off": "disable"}),
                      "interval": ("INT", {"default": 1, "min": 1, "max": 10000}),
-                     "ommit_start_latent": ("BOOLEAN", {"default": False, "label_on": "True", "label_off": "False"}),
+                     "omit_start_latent": ("BOOLEAN", {"default": False, "label_on": "True", "label_off": "False"}),
                      },
                 "optional": {"prev_progress_latent_opt": ("LATENT",), }
                 }
@@ -84,10 +84,10 @@ class KSamplerAdvanced_progress(a1111_compat.KSamplerAdvanced_inspire):
     RETURN_TYPES = ("LATENT", "LATENT")
     RETURN_NAMES = ("latent", "progress_latent")
 
-    def sample(self, model, add_noise, noise_seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, start_at_step, end_at_step, noise_mode, return_with_leftover_noise, interval, ommit_start_latent, prev_progress_latent_opt=None):
+    def sample(self, model, add_noise, noise_seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, start_at_step, end_at_step, noise_mode, return_with_leftover_noise, interval, omit_start_latent, prev_progress_latent_opt=None):
         sampler = a1111_compat.KSamplerAdvanced_inspire()
 
-        if ommit_start_latent:
+        if omit_start_latent:
             result = []
         else:
             result = [latent_image['samples']]
