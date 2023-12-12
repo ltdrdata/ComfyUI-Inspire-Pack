b65d4cdb5b7515e75b1b793622876dca094f02a3
Dr.Lt.Data <dr.lt.data@gmail.com>
2023-12-12 14:49:09 +0900

    improve: overhaul scripting of Concat Conditionings flex feature

--- js/inspire-flex.js
+++ js/inspire-flex.js
@@ -1,127 +1,202 @@
 import { ComfyApp, app } from "../../scripts/app.js";
 
 export function register_concat_conditionings_with_multiplier_node(nodeType, nodeData, app) {
-    if (nodeData.name === 'ConcatConditioningsWithMultiplier //Inspire') {
-        var input_name = "conditioning";
+	if (nodeData.name === 'ConcatConditioningsWithMultiplier //Inspire') {
+		var input_name = "conditioning";
 
-        const onConnectionsChange = nodeType.prototype.onConnectionsChange;
-        nodeType.prototype.onConnectionsChange = async function (type, index, connected, link_info) {
-            if(!link_info || link_info.type != 'CONDITIONING')
-                return;
-
-            let conditionings_count = 0;
-
-            let unconnected = [];
-            for(let i in this.inputs) {
-            	if(i == index)
-            		continue;
-
-                let input = this.inputs[i];
-                if(input.name.startsWith('conditioning')) {
-                    if(input.link == undefined)
-                        unconnected.push(i);
-
-                    conditionings_count += 1;
-                }
-            }
-
-			let self = this;
-            function renames() {
-                let con_i = 1;
-
-                let rename_map = {};
-
-                for(let i in self.inputs) {
-                    let input = self.inputs[i];
-                    if(input.name.startsWith('conditioning')) {
-                    	let orig_i = Number(input.name.substring(12));
-                    	if(orig_i != con_i) {
-                    		rename_map[orig_i] = con_i;
-                    		input.name = 'conditioning'+con_i;
-                    	}
-						con_i++;
-                    }
-                }
-
-                // update multiplier input
-				for(let i in self.inputs) {
-                    let input = self.inputs[i];
-                    if(input.name.startsWith('multiplier')) {
-                    	let orig_i = Number(input.name.substring(10));
-                    	if(rename_map[orig_i]) {
-                    		input.name = 'multiplier'+rename_map[orig_i];
-                    	}
-                    }
-                }
-
-                // update multiplier widget
-				for(let i in self.widgets) {
-                    let w = self.widgets[i];
-                    if(w.name.startsWith('multiplier')) {
-                    	let orig_i = Number(w.name.substring(10));
-                    	if(rename_map[orig_i]) {
-                    		w.name = 'multiplier'+rename_map[orig_i];
-                    	}
-                    }
-                }
-
-                return con_i;
-            }
-
-            if(!connected) {
-                // remove 1
-                let index = Number(this.inputs[unconnected[0]].name.substring(12));
-                this.removeInput(unconnected[0]);
-
-                let target_name = `multiplier${index}`;
-                // remove strength from slot
-                for(let i in this.inputs) {
-                    let input = this.inputs[i];
-                    if(input.name.startsWith(target_name)) {
-                    	if(input.link) {
-                    		let link = app.graph.links[input.link];
-                        	const node = app.graph.getNodeById(link.origin_id);
-                        	let x = node.outputs[link.origin_slot].links.findIndex((w) => w == input.link);
-                        	node.outputs[link.origin_slot].links.splice(x, 1);
-                    		await this.disconnectInput(i);
-                    		app.graph.links.splice(input.link, 1);
-						}
-                        await this.removeInput(i);
-                        break;
-                    }
-                }
-
-                const widget_index = this.widgets.findIndex((w) => w.name == target_name);
-                this.widgets.splice(widget_index, 1);
-
-                let i = renames();
-            }
-            else {
-				const stackTrace = new Error().stack;
-
-				if(stackTrace.includes('loadGraphData'))
+		const onConnectionsChange = nodeType.prototype.onConnectionsChange;
+		let this_handler = async function (type, index, connected, link_info) {
+			let last_state = this.state_change_handling;
+			try {
+				this.state_change_handling = true;
+				if(!link_info || link_info.type != 'CONDITIONING')
 					return;
 
-				if(unconnected.length > 1) {
-					// remove garbage slots
-					unconnected = unconnected.reverse();
-					// TODO: ...
+				let self = this;
+
+				function get_input_count(prefix, linked_only) {
+					let cnt = 0;
+					for(let i in self.inputs) {
+						if(linked_only && !self.inputs[i].link)
+							continue;
+
+						if(self.inputs[i].name.startsWith(prefix))
+							cnt+=1;
+					}
+
+					return cnt;
 				}
 
-                // add 1
-                let i = renames();
-                this.addInput(`conditioning${i}`, this.outputs[0].type);
-                let config = { min: 0, max: 10, step: 0.1, round: 0.01, precision: 2 };
-                let widget = await this.addWidget("number", `multiplier${i}`, 1.0, function (v) {
-					if (config.round) {
-						this.value = Math.round(v/config.round)*config.round;
-					} else {
-						this.value = v;
+				function get_widget_count(prefix) {
+					let cnt = 0;
+					for(let i in self.widgets) {
+						if(self.widgets[i].name.startsWith(prefix))
+							cnt+=1;
 					}
-				}, config);
-            }
 
-            await this.setSize( this.computeSize() );
-        }
-    }
+					return cnt;
+				}
+
+				function get_unconnected() {
+					let unconnected = [];
+					for(let i in self.inputs) {
+						let input = self.inputs[i];
+						if(input.name.startsWith('conditioning')) {
+							if(input.link == undefined)
+								unconnected.push(i);
+						}
+					}
+
+					return unconnected;
+				}
+
+				let unconnected = get_unconnected();
+
+				function renames() {
+					let con_i = 1;
+
+					let rename_map = {};
+
+					for(let i in self.inputs) {
+						let input = self.inputs[i];
+						if(input.name.startsWith('conditioning')) {
+							let orig_i = Number(input.name.substring(12));
+							if(orig_i != con_i) {
+								rename_map[orig_i] = con_i;
+								input.name = 'conditioning'+con_i;
+							}
+							con_i++;
+						}
+					}
+
+					// update multiplier input
+					for(let i in self.inputs) {
+						let input = self.inputs[i];
+						if(input.name.startsWith('multiplier')) {
+							let orig_i = Number(input.name.substring(10));
+							if(rename_map[orig_i]) {
+								input.name = 'multiplier'+rename_map[orig_i];
+							}
+						}
+					}
+
+					// update multiplier widget
+					for(let i in self.widgets) {
+						let w = self.widgets[i];
+						if(w.name.startsWith('multiplier')) {
+							let orig_i = Number(w.name.substring(10));
+							if(rename_map[orig_i]) {
+								w.name = 'multiplier'+rename_map[orig_i];
+							}
+						}
+					}
+
+					return con_i;
+				}
+
+				function remove_multiplier_link(i, link_id) {
+						let link = app.graph.links[link_id];
+						const node = app.graph.getNodeById(link.origin_id);
+						let x = node.outputs[link.origin_slot].links.findIndex((w) => w == link_id);
+						node.outputs[link.origin_slot].links.splice(x, 1);
+						self.disconnectInput(i);
+						app.graph.links.splice(link_id, 1);
+				}
+
+				async function remove_target_multiplier(target_name) {
+					// remove strength from slot
+					for(let i in self.inputs) {
+						let input = self.inputs[i];
+						if(input.name.startsWith(target_name)) {
+							if(input.link) {
+								remove_multiplier_link(i, input.link);
+							}
+							await self.removeInput(i);
+							break;
+						}
+					}
+
+					const widget_index = self.widgets.findIndex((w) => w.name == target_name);
+					self.widgets.splice(widget_index, 1);
+				}
+
+				async function remove_garbage() {
+					let unconnected = get_unconnected();
+
+					// remove unconnected conditionings
+					while(unconnected.length > 0) {
+						let last_one = unconnected.reverse()[0];
+						self.removeInput(last_one);
+						unconnected = get_unconnected();
+					}
+
+					// remove dangling multipliers
+					let conds = new Set();
+					let muls = new Set();
+					for(let i in self.inputs) {
+						let input = self.inputs[i];
+						if(input.link && input.name.startsWith('conditioning')) {
+							let index = Number(input.name.substring(12));
+							conds.add(index);
+						}
+						else if(input.name.startsWith('multiplier')) {
+							let index = Number(input.name.substring(10));
+							muls.add(index);
+						}
+					}
+					for(let i in self.widgets) {
+						let index = Number(self.widgets[i].name.substring(10));
+						muls.add(index);
+					}
+
+					let dangling_muls = [...muls].filter(x => !conds.has(x));
+					while(dangling_muls.length > 0) {
+						let remove_target = dangling_muls.pop();
+						let target_name = `multiplier${remove_target}`;
+						await remove_target_multiplier(target_name);
+					}
+				}
+
+				async function ensure_multipliers() {
+					let ncon = get_input_count('conditioning', true);
+					let nmul = get_input_count('multiplier', false) + get_widget_count('multiplier');
+
+					if(ncon == 0 && nmul == 0)
+						ncon = 1;
+
+					for(let i = nmul+1; i<=ncon; i++) {
+						let config = { min: 0, max: 10, step: 0.1, round: 0.01, precision: 2 };
+						let widget = await self.addWidget("number", `multiplier${i}`, 1.0, function (v) {
+							if (config.round) {
+								self.value = Math.round(v/config.round)*config.round;
+							} else {
+								self.value = v;
+							}
+						}, config);
+					}
+				}
+
+				async function ensure_inputs() {
+					if(get_unconnected() == 0) {
+						let con_i = renames();
+						self.addInput(`conditioning${con_i}`, self.outputs[0].type);
+					}
+				}
+
+				if(!Error().stack.includes('pasteFromClipboard')) {
+					await remove_garbage();
+					await ensure_inputs();
+				}
+
+				await ensure_multipliers();
+
+				await this.setSize( this.computeSize() );
+			}
+			finally {
+				this.state_change_handling = last_state;
+			}
+		}
+
+		nodeType.prototype.onConnectionsChange = this_handler;
+	}
 }
