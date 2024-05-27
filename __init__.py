e255ff46fbe156881217dbd63d14194a4aee1324
Dr.Lt.Data <dr.lt.data@gmail.com>
2024-05-28 00:27:55 +0900

    fix: ImageListToImageBatch - crash when input is single image.

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@
 
 import importlib
 
-version_code = [0, 72]
+version_code = [0, 72, 1]
 version_str = f"V{version_code[0]}.{version_code[1]}" + (f'.{version_code[2]}' if len(version_code) > 2 else '')
 print(f"### Loading: ComfyUI-Inspire-Pack ({version_str})")
 
